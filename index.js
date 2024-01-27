117c35ed1a7d4fed28271c5306b9aba5b394d58c
Ashvin dewdhory <adewdho@hotmail.com>
2024-01-27 23:54:07 +0400

    Load to database

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const filePath = 'CPI Timeseries.csv';
+const axios = require('axios');
+const mysql = require('mysql2/promise');
 
+const worldBankApiUrl = 'http://api.worldbank.org/V2';
+const worldBankIndicators = [ 'ER.FSH.PROD.MT',   // Total fisheries production (metric tons)
+'AG.SRF.TOTL.K2',   // Agricultural land (sq. km)
+'IC.REG.DURS',      // Time required to start a business (days)
+'IC.BUS.NREG',      // New businesses registered (number)
+'SL.AGR.EMPL.ZS',   // Employment in agriculture (% of total employment) (modelled ILO estimate)
+'SL.EMP.SELF.ZS'    // Self-employed, total (% of total employment) (modelled ILO estimate)
+]; // Add more indicators as needed
+
+const worldBankCountriesCodes = [ 'SYC',   // Seychelles
+'MDV',   // Maldives
+'CIV',   // Côte d'ivoire
+'KEN',   // Kenya
+'LKA',   // Sri Lanka
+'IND',   // India
+'SGP',   // Singapore
+'AUS',   // Australia
+'DNK',   // Denmark
+'FIN'    // Finland
+];
+
+// database connection
+
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'macroeconomic_analyser',
+};
 
 function extractData(filePath) {
     return new Promise((resolve, reject) => {
@@ -22,81 +53,98 @@ function extractData(filePath) {
 
 // Step 2: Transform Data
 function transformData(data) {
-    // For simplicity, transform data by selecting a subset of columns and filtering by year (2020 to 2015)
-    const transformedData = data
-        //.filter((row) => row.Year >= 2015 && row.Year <= 2020)
-        .map((row) => {
-            return {
-                Country: row.Country,
-                'CPI score 2020': row['CPI score 2020'],
-                'CPI score 2019': row['CPI score 2019'],
-                'CPI score 2018': row['CPI score 2018'],
-                'CPI score 2017': row['CPI score 2017'],
-                'CPI score 2016': row['CPI score 2016'],
-                'CPI score 2015': row['CPI score 2015'],
-                'CPI score 2014': row['CPI score 2014'],
-                'CPI score 2013': row['CPI score 2013'],
-                'CPI score 2012': row['CPI score 2012']
-            };
-        });
+    // For simplicity, transform data by selecting a subset of columns and filtering by year (2020 to 2012)
+    const transformedData = [];
 
+    data.map((row) => {
+
+        // Loop through the years in the row
+        for (let year = 2020; year >= 2012; year--) {
+
+            const obj = {
+                IndicatorName : 'Corruption Perception Index',
+                IndicatorCode : 'CPI',
+                CountryCode: row.ISO3,
+                Year: year,
+                Value: row[`CPI Score ${year}`]||row[`CPI score ${year}`],
+                Rank: row[`Rank ${year}`],
+            }
+
+            transformedData.push(obj);
+        }
+
+        
+    });
+    //console.log(transformedData)
     return transformedData;
 }
 
-async function runDataPipeline(filePath) {
-    try {
-        // Step 1: Extract Data
-        const rawData = await extractData(filePath);
-        //console.log(rawData);
-
-        // Step 2: Transform Data
-        const transformedData = transformData(rawData);
-        console.log(transformedData);
-
-        // Step 3: Load Data
-        //loadData(transformedData);
-    } catch (error) {
-        console.error('Error in data pipeline:', error);
-    }
-}
-
-const axios = require('axios');
-
-const worldBankApiUrl = 'http://api.worldbank.org/V2';
-const worldBankIndicators = [ 'ER.FSH.PROD.MT',   // Total fisheries production (metric tons)
-'AG.SRF.TOTL.K2',   // Agricultural land (sq. km)
-'IC.REG.DURS',      // Time required to start a business (days)
-'IC.BUS.NREG',      // New businesses registered (number)
-'SL.AGR.EMPL.ZS',   // Employment in agriculture (% of total employment) (modelled ILO estimate)
-'SL.EMP.SELF.ZS'    // Self-employed, total (% of total employment) (modelled ILO estimate)
-]; // Add more indicators as needed
-
-// Step 1: Extract Data from World Bank API
+// Step 3: Extract Data from World Bank API
 async function extractWorldBankData() {
     try {
-        const responses = (await axios.get(`${worldBankApiUrl}/country/CHN;TCD/indicator/${worldBankIndicators.join(';')}?format=json&date=2017:2022&source=2`)).data[1];
-        console.log(responses);
-
+        const responses = (await axios.get(`${worldBankApiUrl}/country/${worldBankCountriesCodes.join(';')}/indicator/${worldBankIndicators.join(';')}?format=json&date=2019:2022&source=2`)).data[1];
+        
         const data = responses.reduce((result, response, index) => {
-            console.log(result, response, index);
-            const indicator = worldBankIndicators[index];
+            //console.log(result, response, index);
 
-           /* response.forEach((entry) => {
+            responses.forEach(entry => {
                 result.push({
-                    Country: entry.country.value,
-                    Indicator: indicator,
+                    IndicatorName : response.indicator.value,
+                    IndicatorCode : response.indicator.id,
+                    CountryCode: entry.countryiso3code,
+                    Year: entry.date,
                     Value: entry.value || null,
+                    Rank : null
                 });
-            });*/
-
+            });
+            //console.log(result);
             return result;
         }, []);
-
+        //console.log(data);
         return data;
     } catch (error) {
         throw error;
     }
 }
 
-//runDataPipeline(filePath);
-extractWorldBankData();
+// Step 3: Load Data
+async function loadData(data) {
+    const connection = await mysql.createConnection(dbConfig);
+    
+    try {
+
+        // Insert data into the table
+        for (const row of data) {
+            await connection.query(`
+                INSERT INTO indicator_info (countryCode, year, value, Rank, name, indicatorCode)
+                VALUES (?, ?, ?, ?, ?, ?)
+            `, [row.CountryCode, row.Year, row.Value, row.Rank, row.IndicatorName, row.IndicatorCode]);
+        }
+
+        console.log('CPI data loaded into the MySQL database successfully.');
+    } catch (error) {
+        console.error('Error loading CPI data into the database:', error);
+    } finally {
+        // Close the database connection
+        await connection.end();
+    }
+}
+
+async function runDataPipeline(filePath) {
+    try {
+        // Step 1: Extract Data
+        const rawData = await extractData(filePath);
+
+        // Step 2: Transform Data
+        const transformedData = transformData(rawData);
+        const worldBankData = await extractWorldBankData();
+
+        // Step 3: Load Data
+        loadData([...worldBankData, ...transformedData]);
+
+    } catch (error) {
+        console.error('Error in data pipeline:', error);
+    }
+}
+
+runDataPipeline(filePath);
