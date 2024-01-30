51ea2c30c6984797768820c1f37b67dd6cce0858
Ashvin dewdhory <adewdho@hotmail.com>
2024-01-30 23:14:44 +0400

    Readme and refactoring

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Macroeconomic-Analyser
 
-https://app.powerbi.com/reportEmbed?reportId=b31be9ac-0db6-488d-88c0-7b072cc0bb57&autoAuth=true&ctid=0af08255-c76b-48c0-8dc2-0dfdbf7bae40
+## Data sources
+CPI Timeseries.csv
+World bank data
+
+## Technologies
+1. JavaScript and MySql database: They work well together in a data pipeline: with Node.js, handles asynchronous data processing and API interactions tasks, while MySQL provides a structured backend for efficient data storage and retrieval.
+2. PowerBI (recommended)
+
+## Design implementation considerations
+1. Data source integration: 
+    - Understand the World Bank API structure and endpoints and handle pagination
+    - Ensure that the CSV file follows a consistent structure
+2. Data transformation:
+    - Create a mapping between the data from the World Bank API and the local CSV file to ensure consistency in the schema
+3. Error handling and logging
+
+## Data handling
+The ETL (extract, transform and load) process is used to combine data from the Corruption Perception Index Data Set and the World Development Indicators into a single, consistent data store that is loaded into a MySQL database. Two required tables are created namely countries and indicator_info having a one-to-many relationship on the country code column.
+
+## PowerBI report
+https://app.powerbi.com/view?r=eyJrIjoiOTQxYTY0MDQtOTM1NS00MWI1LTk0M2MtNmVjYjVhMjJmZGU4IiwidCI6IjBhZjA4MjU1LWM3NmItNDhjMC04ZGMyLTBkZmRiZjdiYWU0MCJ9
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const csv = require('csv-parser');
-const filePath = 'CPI Timeseries.csv';
 const axios = require('axios');
 const mysql = require('mysql2/promise');
 const { totalmem } = require('os');
 
+const filePath = 'CPI Timeseries.csv';
 const worldBankApiUrl = 'http://api.worldbank.org/V2';
-const worldBankIndicators = [ 'ER.FSH.PROD.MT',   // Total fisheries production (metric tons)
-'AG.SRF.TOTL.K2',   // Agricultural land (sq. km)
-'IC.REG.DURS',      // Time required to start a business (days)
-'IC.BUS.NREG',      // New businesses registered (number)
-'SL.AGR.EMPL.ZS',   // Employment in agriculture (% of total employment) (modelled ILO estimate)
-'SL.EMP.SELF.ZS'    // Self-employed, total (% of total employment) (modelled ILO estimate)
-]; // Add more indicators as needed
+const worldBankIndicators = [
+    'ER.FSH.PROD.MT',   // Total fisheries production (metric tons)
+    'AG.SRF.TOTL.K2',   // Agricultural land (sq. km)
+    'IC.REG.DURS',      // Time required to start a business (days)
+    'IC.BUS.NREG',      // New businesses registered (number)
+    'SL.AGR.EMPL.ZS',   // Employment in agriculture (% of total employment) (modelled ILO estimate)
+    'SL.EMP.SELF.ZS'    // Self-employed, total (% of total employment) (modelled ILO estimate)
+];
 
 // database connection
 const dbConfig = {
@@ -22,7 +23,9 @@ const dbConfig = {
     database: 'macroeconomic_analyser',
 };
 
-function extractData(filePath) {
+// Extract CPI Timeseries.csv Data
+function extractCsvData(filePath) {
+    console.log('Starting csv extraction');
     return new Promise((resolve, reject) => {
         const data = [];
         fs.createReadStream(filePath)
@@ -39,16 +42,12 @@ function extractData(filePath) {
     });
 }
 
-// Step 2: Transform Data
-function transformData(data) {
-    // For simplicity, transform data by selecting a subset of columns and filtering by year (2020 to 2012)
+function transformCsvData(data) {
+    console.log('Transforming csv data');
     const transformedData = [];
 
     data.map((row) => {
-
-        // Loop through the years in the row
         for (let year = 2020; year >= 2012; year--) {
-
             const obj = {
                 IndicatorName : 'Corruption Perception Index',
                 IndicatorCode : 'CPI',
@@ -57,18 +56,19 @@ function transformData(data) {
                 Value: row[`CPI Score ${year}`]||row[`CPI score ${year}`],
                 Rank: row[`Rank ${year}`],
             }
-
             transformedData.push(obj);
         }
-
-        
     });
-    //console.log(transformedData)
     return transformedData;
 }
 
-// Step 3: Extract Data from World Bank API
+async function getWorldBankData(page) {
+    const responses = (await axios.get(`${worldBankApiUrl}/country/all/indicator/${worldBankIndicators.join(';')}?format=json&date=2019:2022&source=2&per_page=100&page=${page}`));
+    return responses;
+}
+
 async function extractWorldBankData() {
+    console.log('Extracting world bank data');
     try {
         let page = 1;
         let totalPages = 0;
@@ -76,7 +76,6 @@ async function extractWorldBankData() {
 
         do {
             const responses = await getWorldBankData(page);
-            //console.log(responses);
             totalPages = responses.data[0].pages;
 
             results.push(...responses.data[1].map((response) => {
@@ -93,7 +92,7 @@ async function extractWorldBankData() {
             page = page + 1;
         }
         while (page <= totalPages);
-        
+
         //console.log(data);
         return results;
     } catch (error) {
@@ -101,40 +100,30 @@ async function extractWorldBankData() {
     }
 }
 
-async function getWorldBankData(page) {
-    const responses = (await axios.get(`${worldBankApiUrl}/country/all/indicator/${worldBankIndicators.join(';')}?format=json&date=2019:2022&source=2&per_page=100&page=${page}`));
-    return responses;
-}
-
-// Step 3: Load Data
 async function loadData(data) {
+    console.log('Inserting into databse');
     const connection = await mysql.createConnection(dbConfig);
     
     try {
-
-        // Insert data into the table
         for (const row of data) {
             await connection.query(`
                 INSERT INTO indicator_info (countryCode, year, value, Rank, name, indicatorCode)
                 VALUES (?, ?, ?, ?, ?, ?)
             `, [row.CountryCode, row.Year, row.Value, row.Rank, row.IndicatorName, row.IndicatorCode]);
         }
-
         console.log('CPI data loaded into the MySQL database successfully.');
     } catch (error) {
         console.error('Error loading CPI data into the database:', error);
     } finally {
-        // Close the database connection
         await connection.end();
     }
 }
 
-// Step 4: Update Rank Column
 async function updateRank() {
+    console.log('Updating rank field');
     const connection = await mysql.createConnection(dbConfig);
     
     try {
-
         // Update Rank field in indicator_info table
         await connection.query(`
             UPDATE INDICATOR_INFO i1
@@ -149,28 +138,25 @@ async function updateRank() {
 
         console.log('Rank field in indicator_info table updated successfully.');
     } catch (error) {
-        console.error('Error loading CPI data into the database:', error);
+        console.error('Error updating Rank column in indicator_info table.', error);
     } finally {
-        // Close the database connection
         await connection.end();
     }
 }
 
 async function runDataPipeline(filePath) {
     try {
-        // Step 1: Extract Data
-        const rawData = await extractData(filePath);
-
-        // Step 2: Transform Data
-        const transformedData = transformData(rawData);
+        console.log('Starting data pipeline');
+        const csvData = await extractCsvData(filePath);
+        const transformedCsvData = transformCsvData(csvData);
         const worldBankData = await extractWorldBankData();
 
-        // Step 3: Load Data
-        await loadData([...worldBankData, ...transformedData]);
+        // Insert data in indicator_info table
+        await loadData([...worldBankData, ...transformedCsvData]);
 
-        //Step 4: Update Rank column in indicator_info table
+        // Update Rank column in indicator_info table
         await updateRank(); 
-
+        console.log('Ending data pipeline');
     } catch (error) {
         console.error('Error in data pipeline:', error);
     }
